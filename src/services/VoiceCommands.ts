310f4a87443b8b3a809d067f5ca2eb409e9d3647
Arky <20231736@nbsc.edu.ph>
2025-06-14 20:59:11 +0800

    minor fixes

--- src/services/VoiceCommands.ts
+++ src/services/VoiceCommands.ts
@@ -11,7 +11,7 @@ class VoiceCommands {
 
   private setupCommands() {
     this.voiceService.startListening((command) => {
-      if (command.includes('Hello')) {
+      if (command.includes('hello')) {
         this.playHaiResponse();
       }
     });
