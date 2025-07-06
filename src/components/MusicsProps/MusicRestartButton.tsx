cbd20232e973afd549ba8d9b44ac5bd6b973becc
Arky <20231736@nbsc.edu.ph>
2025-07-06 21:46:14 +0800

    Updated MusicRestartButton.tsx

--- src/components/MusicsProps/MusicRestartButton.tsx
+++ src/components/MusicsProps/MusicRestartButton.tsx
@@ -1,9 +1,19 @@
 import { IonButton, IonIcon } from '@ionic/react';
 import { refresh } from 'ionicons/icons';
 
-const MusicRestartButton: React.FC = () => {
+interface MusicRestartButtonProps {
+  onRestart: () => void;
+  disabled?: boolean;
+}
+
+const MusicRestartButton: React.FC<MusicRestartButtonProps> = ({ onRestart, disabled = false }) => {
   return (
-    <IonButton color="medium" shape="round">
+    <IonButton 
+      color="medium" 
+      shape="round"
+      onClick={onRestart}
+      disabled={disabled}
+    >
       <IonIcon icon={refresh} />
     </IonButton>
   );
